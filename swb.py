a1283b20f69d752b005b2c4bf41dc6ba038c42cf
asd <asd@asd.com>
2025-12-16 17:15:53 +0100

    small change

--- swb.py
+++ swb.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 
-
 import sys
 import argparse
 import random
@@ -11,7 +10,7 @@ from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from urllib.parse import urlencode
 
-#def input_ruby:
+# move to separate function but keep generic in main
 
 def main():
   session = requests.Session()
@@ -41,7 +40,8 @@ def main():
 
       if input_tag.get("type") == "text" and input_tag.has_attr("required"):
           payload[name] = "test"
-
+# if there already is a predefined value put in another list and do nothign...
+# this will cause problems in future
   for input_tag in form.find_all("input"):
       name = input_tag.get("name")
       value = input_tag.get("value", "")
@@ -57,10 +57,11 @@ def main():
             payload[k] = "FUZZVALUE"
             encoded = urlencode(list(payload.items()))
             encoded = encoded.replace('FUZZVALUE', x.strip())
-#            print("Payload:", encoded)
+
             start = time.monotonic()
             post_response = session.post(post_url, data=encoded, timeout=60)
             elapsed = time.monotonic() - start
+
             if elapsed > 5:
                 print(f"⚠️ Slow response successuful Ruby SSTI: {elapsed:.2f}s")
                 print("Payload:", encoded)
@@ -70,12 +71,6 @@ def main():
 #            print(post_response.text)
         else:
             continue
-#        exit()
-
-  
-#  print(post_response.text[:500])
-
-
 
 
 if __name__=="__main__":
