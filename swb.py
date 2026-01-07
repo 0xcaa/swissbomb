a0d10a04fa9e3848d1750417df4045161d718de9
asd <asd@asd.com>
2026-01-08 00:24:01 +0100

    subdomain enumerator with ratelimit

--- swb.py
+++ swb.py
@@ -13,7 +13,9 @@ import urllib.parse
 from urllib.parse import urljoin
 from urllib.parse import urlencode
 from urllib.parse import urlparse
+from ratelimit import limits, sleep_and_retry
 
+RESET = '\033[0m' # return to standard terminal text color
 BLACK = '\033[30m'
 RED = '\033[31m'
 GREEN = '\033[32m'
@@ -30,9 +32,6 @@ BRIGHT_BLUE = '\033[94m'
 BRIGHT_MAGENTA = '\033[95m'
 BRIGHT_CYAN = '\033[96m'
 WHITE = '\033[97m'
-
-RESET = '\033[0m' # return to standard terminal text color
-
 BACKGROUND_BLACK = '\033[40m'
 BACKGROUND_RED = '\033[41m'
 BACKGROUND_GREEN = '\033[42m'
@@ -50,6 +49,15 @@ BACKGROUND_BRIGHT_MAGENTA = '\033[105m'
 BACKGROUND_BRIGHT_CYAN = '\033[106m'
 BACKGROUND_WHITE = '\033[107m'
 
+# rate limit
+FAST = 5
+ONE_MINUTE = 60
+FIVE_MINUTES = 300
+FIFTEEN_MINUTES = 900
+ONE_HOUR = 3600
+
+TIMEOUT = 5
+
 
 
 ##Todo
@@ -57,25 +65,53 @@ BACKGROUND_WHITE = '\033[107m'
 # git repository finder function
 # pass wordlist as filename to exploit function
 
+
+@sleep_and_retry
+@limits(calls=1, period=FAST)
+def send_request(target, header, result):
+    print("testing this: ", target)
+    print("and this header: ", header)
+    try:
+        random_response = requests.get(
+            target,
+            timeout=TIMEOUT,
+            headers={"Host": header},
+            verify=False,#todo add https
+            allow_redirects=True)
+        if random_response.status_code < 500:
+            result["wildcard_detected"] = True
+            result["target"] = True
+            result["target"] = target
+            print(RED + result + RESET)
+            return result
+    except requests.exceptions.HTTPError as e:
+        print(RED)
+        print(e)
+        print(RESET)
+    except requests.RequestException as e:
+        print(RED)
+        print(e)
+        print(RESET)
+        print(YELLOW + "NO wildcard detected on", target + RESET)
+    return result
+
 def random_subdomain(length: int = 16) -> str:
     return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
-
 
 def enumerate_subdomain(url: str, domain: str, timeout: int = 5):
     """
     :return: dict with existence and wildcard info
-    """
+   """
 
 #    target = f"https://{subdomain}.{domain}"
     target = url
-    # bad http hardcode
+    random_header = f"{random_subdomain()}.{domain}"
     if "https" in url:
-        random_target = f"https://{random_subdomain()}.{domain}"
+        random_target = f"https://{random_header}"
     else:
-        random_target = f"http://{random_subdomain()}.{domain}"
+        random_target = f"http://{random_header}"
 #    random_target = f"https://{random_subdomain()}.{domain}"
 #    random_target = f"https://test2.{domain}"
-    print(random_target)
 
     result = {
         "target": target,
@@ -84,48 +120,18 @@ def enumerate_subdomain(url: str, domain: str, timeout: int = 5):
         "status_code": None
     }
 
-
     # Wildcard detection
-    try:
-        random_response = requests.get(
-            random_target,
-            timeout=timeout,
-            allow_redirects=True
-        )
-        if random_response.status_code < 500:
-            result["wildcard_detected"] = True
-            result["target"] = True
-            return result
-    except requests.RequestException:
-        pass
-
+    print("wildcard test: ", random_target)
+    result = send_request(random_target, random_header, result)
 
     # loop through wordlist of subdomains
     with open("wordlist.txt") as f:
         for word in f:
-            try:
-                target = f"http://{word.strip()}.{domain}"
-                print(target)
-                response = requests.get(target, timeout=timeout, allow_redirects=True)
-                format_response(response, word)
-                result["status_code"] = response.status_code
-                result["target"] = target
-                print(result)
-
-                # Treat any HTTP response as "exists"
-                if response.status_code < 500:
-                    result["exists"] = True
-                    format_response(response, word)
-                    print(result)
-                else:
-                    print(result)
-
-            except requests.RequestException:
-                print(result)
-                pass
+            target = f"http://{word.strip()}.{domain}"
+            header = f"{word.strip()}.{domain}"
+            result = send_request(target, header, result)
 
     return result
-
 
 def format_response(response, payload):
     if response.status_code > 300 and response.status_code < 500:
@@ -142,83 +148,83 @@ def cypher(url):
 
 #  url = "http://cypher.htb/login"
 
-  session = requests.Session()
-  response = session.get(url)
+    session = requests.Session()
+    response = session.get(url)
 
-  soup = BeautifulSoup(response.text, "html.parser")
+    soup = BeautifulSoup(response.text, "html.parser")
 
-  form = soup.find("form")
-  action = form.get("action")
-  post_url = urljoin(url, action)
-  print("This is the post URL: ", post_url)
+    form = soup.find("form")
+    action = form.get("action")
+    post_url = urljoin(url, action)
+    print("This is the post URL: ", post_url)
 
-  c = [("admin", "admin")]
+    c = [("admin", "admin")]
 
-  with open("wordlist.txt") as f:
-      for word in f:
-          for u, p in c:
-              formatted = f'{{"username":"{word.strip()}","password":"{p}"}}'
-              post_response = session.post(post_url, data=formatted, timeout=60)
-              format_response(post_response, urllib.parse.quote_plus(formatted))
-              time.sleep(1)
-              formatted = f'{{"username":"{u}","password":"{word.strip()}"}}'
-              post_response = session.post(post_url, data=formatted, timeout=60)
-              format_response(post_response, urllib.parse.quote_plus(formatted))
-              time.sleep(1)
+    with open("wordlist.txt") as f:
+        for word in f:
+            for u, p in c:
+                formatted = f'{{"username":"{word.strip()}","password":"{p}"}}'
+                post_response = session.post(post_url, data=formatted, timeout=60)
+                format_response(post_response, urllib.parse.quote_plus(formatted))
+                time.sleep(1)
+                formatted = f'{{"username":"{u}","password":"{word.strip()}"}}'
+                post_response = session.post(post_url, data=formatted, timeout=60)
+                format_response(post_response, urllib.parse.quote_plus(formatted))
+                time.sleep(1)
 
 
 def weightedgrade(url):
 
-  session = requests.Session()
+    session = requests.Session()
 
-#  url = "http://10.10.11.253/weighted-grade"
-  response = session.get(url)
+  #  url = "http://10.10.11.253/weighted-grade"
+    response = session.get(url)
 
-  soup = BeautifulSoup(response.text, "html.parser")
+    soup = BeautifulSoup(response.text, "html.parser")
 
-  form = soup.find("form")
-  action = form.get("action")
-  post_url = urljoin(url, action)
-  print("This is the post URL: ", post_url)
+    form = soup.find("form")
+    action = form.get("action")
+    post_url = urljoin(url, action)
+    print("This is the post URL: ", post_url)
 
-  payload = {}
+    payload = {}
 
 
-  for input_tag in form.find_all("input"):
-      name = input_tag.get("name")
-      value = input_tag.get("value", "")
+    for input_tag in form.find_all("input"):
+        name = input_tag.get("name")
+        value = input_tag.get("value", "")
 
-      if name:
-          payload[name] = value
-      if not value and input_tag.get("type") == "number" and input_tag.has_attr("required"):
-          payload[name] = 20
+        if name:
+            payload[name] = value
+        if not value and input_tag.get("type") == "number" and input_tag.has_attr("required"):
+            payload[name] = 20
 
-      if not value and input_tag.get("type") == "text" and input_tag.has_attr("required"):
-          payload[name] = "test"
+        if not value and input_tag.get("type") == "text" and input_tag.has_attr("required"):
+            payload[name] = "test"
 
-  with open("wordlist.txt") as f:
-    for x in f:
-      for k, v in payload.items():
-        if payload[k] == "test":
-            temp = payload[k]
-            payload[k] = "FUZZVALUE"
-            encoded_payload = urlencode(list(payload.items()))
-            encoded_payload = encoded_payload.replace('FUZZVALUE', x.strip())
+    with open("wordlist.txt") as f:
+      for x in f:
+        for k, v in payload.items():
+          if payload[k] == "test":
+              temp = payload[k]
+              payload[k] = "FUZZVALUE"
+              encoded_payload = urlencode(list(payload.items()))
+              encoded_payload = encoded_payload.replace('FUZZVALUE', x.strip())
 
-            start = time.monotonic()
-            post_response = session.post(post_url, data=encoded_payload, timeout=60)
-            elapsed = time.monotonic() - start
+              start = time.monotonic()
+              post_response = session.post(post_url, data=encoded_payload, timeout=60)
+              elapsed = time.monotonic() - start
 
-            if elapsed > 5:
-                print(f"⚠️ Slow response successuful Ruby SSTI: {elapsed:.2f}s")
-                print("Full Payload:", encoded_payload)
-                print("payload: ", x)
-                sys.exit()
-            payload[k] = temp
-            print(post_response.status_code)
+              if elapsed > 5:
+                  print(f"⚠️ Slow response successuful Ruby SSTI: {elapsed:.2f}s")
+                  print("Full Payload:", encoded_payload)
+                  print("payload: ", x)
+                  sys.exit()
+              payload[k] = temp
+              print(post_response.status_code)
 #            print(post_response.text)
-        else:
-            continue
+          else:
+              continue
 
 
 def check_ping(hostname) -> bool:
@@ -236,44 +242,41 @@ def check_ping(hostname) -> bool:
 
 def parse_arguments():
 
-  parser = argparse.ArgumentParser(
-          prog='swb.py',
-          description='Swissbomb demo',
-          epilog='this is the epilog')
+    parser = argparse.ArgumentParser(
+            prog='swb.py',
+            description='Swissbomb demo',
+            epilog='this is the epilog')
 
-  parser.add_argument('url', help='the URL of your choice: %(prog)s  http://htburl.htb', type=str)
-  parser.add_argument('--noping', help='skip the default ping check', action='store_true')
-  parser.add_argument('--nosub', help='skip subdomain enumeration', action='store_true')
-  parser.add_argument('-t', '--timeout', help='timeout values in seconds', default=5, type=int)
-  args = parser.parse_args()
-  return args
+    parser.add_argument('url', help='the URL of your choice: %(prog)s  http://htburl.htb', type=str)
+    parser.add_argument('--noping', help='skip the default ping check', action='store_true')
+    parser.add_argument('--nosub', help='skip subdomain enumeration', action='store_true')
+    parser.add_argument('-r', '--rate', help='rate limit FAST, MEDIUM, SLOW', default='FAST', type=str)
+    args = parser.parse_args()
+    return args
 
 def main():
 
-  args = parse_arguments()
-  url = args.url
-  parsed_url = urlparse(url)
-  hostname = parsed_url.hostname
-  timeout = args.timeout
-  print('timeout value: ', timeout)
+    args = parse_arguments()
+    url = args.url
+    parsed_url = urlparse(url)
+    hostname = parsed_url.hostname
 
-  if args.noping:
-      print("Skipping the ping check")
-  else:
-      if not check_ping(hostname):
-          sys.exit()
+    if args.noping:
+        print("Skipping the ping check")
+    else:
+        if not check_ping(hostname):
+            sys.exit()
 
-  print('Target:', url)
-  time.sleep(2)
+    print('Target:', url)
+    time.sleep(2)
 
 # select function to run
-#  print(enumerate_subdomain(url, hostname, timeout))
-  enumerate_subdomain(url, hostname, timeout)
-  sys.exit()
-  cypher(url)
-  sys.exit()
-  weightedgrade()
-  sys.exit()
+    enumerate_subdomain(url, hostname, TIMEOUT)
+    sys.exit()
+    cypher(url)
+    sys.exit()
+    weightedgrade()
+    sys.exit()
 
 if __name__=="__main__":
     main()
